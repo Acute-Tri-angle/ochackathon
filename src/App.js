769673f647331664414007cbe08bd841fe2e0bc6
Acute-Tri-angle <pptran106@student.hbuhsd.edu>
2026-04-18 13:53:49 -0700

    button

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import logo from './carlogo.jpeg';
 import './App.css';
+import { useState } from 'react';
 
+function Button({label, onClick, color = 'red'}) {
+  const buttonstyle = {
+    backgroundColor: color,
+    color: 'white',
+    padding: '10px 20px',
+    border: 'none',
+    borderRadius: '5px',
+    cursor: 'pointer'
+  }
+  return (
+    <button style = {buttonstyle} onClick = {onClick}>
+      {label}
+    </button>
+    
+  );
+}
 
-function App() {
+const App = () => {
+
 return(
 <div>
   <div className = "App-logo"><img src = {logo} alt = "carlogo" width="100" height="100"></img></div>
   <div className = "App">
   <header className = "App-header">car gaem</header>
   </div>
-  <div><button type = "button">click me</button></div>
+  <div><Button label="click me" /></div>
 </div>
-
-  
 );
 
   
