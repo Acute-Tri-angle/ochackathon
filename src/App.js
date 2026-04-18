4a87362174b86ccc09aaae4ef8f647289c880309
Acute-Tri-angle <pptran106@student.hbuhsd.edu>
2026-04-18 15:27:47 -0700

    bugfixes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function Button({label, onClick, color = 'red'}) {
   const buttonstyle = {
     backgroundColor: color,
     color: 'white',
-    padding: '10px 20px',
+    padding: '1vh 60vh',
     border: 'none',
     borderRadius: '5px',
     cursor: 'pointer'
@@ -19,15 +19,24 @@ function Button({label, onClick, color = 'red'}) {
   );
 }
 
+function handleClick(){
+
+}
+
+
 const App = () => {
 
 return(
-<div>
+<div className = "App-bg">
   <div className = "App-logo"><img src = {logo} alt = "carlogo" width="100" height="100"></img></div>
-  <div className = "App">
-  <header className = "App-header">car gaem</header>
+  <div className = "App-centertext">
+  <header className = "App-header">On the Lookout!</header>
   </div>
-  <div><Button label="click me" /></div>
+  <div className = "App-centertext">
+    <div className = "App-whitetext">
+      You are a police officer patroling on an eerie night. Your job is to be on the look out for any strange driving behaviors from other drivers. Can you keep the roads safe? Press the button below to start the night.</div>
+    </div>
+  <div><Button label="start da game" /></div>
 </div>
 );
 
