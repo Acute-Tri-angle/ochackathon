843b272e11b181c02168ed7f135429c1fd255584
Acute-Tri-angle <pptran106@student.hbuhsd.edu>
2026-04-18 13:06:06 -0700

    changed some things

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import logo from './carlogo.jpeg';
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+return(
+<div>
+  <div className = "App-logo"><img src = {logo} alt = "carlogo" width="100" height="100"></img></div>
+  <div className = "App">
+  <header className = "App-header">car gaem</header>
+  </div>
+  <div><button type = "button">click me</button></div>
+</div>
+
+  
+);
+
+  
 }
 
 export default App;
